09819d9c19560a345fc965d61ee7aa5d21710319
nakamura.hiroki <nakamura2465@gmail.com>
2024-03-12 00:01:59 +0900

    fix: COMMAND実行時に指定するパスをtopicからmessageで判定するよう修正した

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on("connect", () => {
 
 client.on("message", (topic, message) => {
   console.log(`${topic}: ${message.toString()}`);
-  const COMMAND = (topic) => `sudo ~/bto_ir_cmd/bto_ir_cmd -e -t  \`cat ~/bto_ir_cmd/codes/${topic.replace("home/", "")}\``
+  const COMMAND = (message) => `sudo ~/bto_ir_cmd/bto_ir_cmd -e -t  \`cat ~/bto_ir_cmd/codes/${message.data}\``
   exec(
     COMMAND(topic),
      (err, stdout, stderr) => {
